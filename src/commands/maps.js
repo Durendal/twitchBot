971683887dd232dd3939addf4d8338f4afd075c3
Durendal <sir.durendal@gmail.com>
2019-12-30 00:12:21 -0600

    Fixed bug where removing a map would prevent people who had voted for it from casting a new ballot

--- src/commands/maps.js
+++ src/commands/maps.js
@@ -2,6 +2,7 @@ const { state } = require('src/config');
 const { mapList } = require('src/utils').maps;
 const { isAdmin } = require('src/utils').admins;
 const { getUserName } = require('src/utils').users;
+const { addLog } = require('src/utils').logging;
 
 function topMap() {
   const result = mapList()
@@ -30,6 +31,14 @@ function delMap(map, context) {
   if(!isAdmin(getUserName(context)))
     return;
   delete state['maps'][map];
+  Object.keys(state['voters'])
+    .forEach(user => {
+      if(state['voters'][user] === map) {
+        addLog(`${user} being cleared with ${map} `);
+        delete state['voters'][user];
+      }
+    });
+
 }
 
 module.exports = {
