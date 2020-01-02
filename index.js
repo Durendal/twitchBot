55aa27786ac0650ce3cc76c6a7fdbaff0fbdc52a
Durendal <sir.durendal@gmail.com>
2020-01-02 16:01:31 -0600

    Subscribed to store in index

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import "app-module-path/register";
 import { client } from 'src/utils/client';
 import { onMessageHandler, onConnectedHandler } from 'src/handlers';
+import store from 'src/state/store';
+
+// Subscribe to redux store
+store.subscribe(() => console.log('Store changed', store.getState()));
 
 // Register our event handlers
 client.on('message', onMessageHandler);
