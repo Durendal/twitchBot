5f029d82228c586ec87633e66c7c9228700c9263
Durendal <sir.durendal@gmail.com>
2020-01-06 00:45:29 -0600

    Documentation

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,9 +1,15 @@
 import { getUserName } from 'src/utils/users';
 
+/**
+  parse a message returning the username of the sender, the command, and any arguments
+  @param {String} msg - The message sent by the user
+  @param {Object} context - The context of the message being sent
+ */
 const parseMessage = (msg, context) => {
   const username = getUserName(context);
   const commandName = msg.trim().split(" ")[0];
   const arguments = msg.trim().split().slice(1);
+
   return {
     username,
     commandName,
