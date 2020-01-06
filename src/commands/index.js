1d889ecdc882927d0fa2013189245ae16aeb88c2
Durendal <sir.durendal@gmail.com>
2020-01-06 14:01:12 -0600

    Updates

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,5 +1,6 @@
 import * as admins from 'src/commands/admins';
 import * as polls from 'src/commands/polls';
+import { client } from 'src/utils/client';
 
 /**
   Object containing all commands for the bot.
@@ -12,27 +13,19 @@ const commands = {};
   @param {Function} fn - The function to execute when command is issued
  */
 const add = (name, fn) => {
-   commands[name] = fn;
-};
-
-/**
-  Execute an issued command
-  @param {String} name - The command to execute
-  @param {Array} args - Any arguments that need to be passed to the command
- */
-const commandSwitch = (name, ...args) => {
-  if (listCommands.includes(name))
-    commands[name](...args);
-  else
-    client.say(target, `Unknown Command: ${name}`);
+  console.log(`Adding: ${name} to command list: `, fn);
+  commands[name.toLowerCase()] = fn;
 };
 
 /**
   List all available commands
  */
 const listCommands = (target) => {
-  const commands = Object.keys(commands).join(", ");
-  client.say(target, `Available commands: ${commands}`);
+  const command_list = Object.keys(commands)
+    .map(command => `!${command.toLowerCase()}`)
+    .join(", ");
+  //client.say(target, `Available commands: ${command_list}`);
+  return command_list;
 };
 
 // Add commands to the commands object
@@ -48,11 +41,25 @@ Object.keys(admins)
     add(admin, admins[admin]);
   });
 
+/**
+  Execute an issued command
+  @param {String} name - The command to execute
+  @param {Array} args - Any arguments that need to be passed to the command
+ */
+const commandSwitch = (name, msg, context, target) => {
+  if (listCommands(target).includes(name)){
+    // Trim ! from command
+    const short_name = name.substring(1);
+    commands[short_name](msg, context, target);
+  }
+  else
+    client.say(target, `Unknown Command: ${name}`);
+};
+
 export {
   admins,
   polls,
   commands,
   commandSwitch,
-  listCommands,
-  add
+  add,
 };
