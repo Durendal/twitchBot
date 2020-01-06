06dac3f7edc20e508dd0b847ed123b194ac0aaa1
Durendal <sir.durendal@gmail.com>
2020-01-06 13:59:56 -0600

    Removed parseMessage from client utils file

--- src/utils/client.js
+++ src/utils/client.js
@@ -3,15 +3,6 @@ import { client as config } from 'src/config';
 
 const client = new tmi.client(config);
 
-/**
-  Parse user output out of a sent message
-  @param {String} msg - The message to parse
- */
-function parseMessage(msg) {
-  return msg.trim().split(" ").slice(1).join(" ");
-}
-
 export {
   client,
-  parseMessage,
 };
