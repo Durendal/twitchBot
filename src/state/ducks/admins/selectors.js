31dac7f0a1e2a2dc9d7398a0a1dc704dba46fa56
Durendal <sir.durendal@gmail.com>
2020-01-06 00:33:05 -0600

    Added selectors to poll and admins ducks modules

--- src/state/ducks/admins/selectors.js
+++ src/state/ducks/admins/selectors.js
@@ -1,7 +1,10 @@
+/* get a list of channels that a user has admin status in */
 const getAdminChannels = (state, username) => state.admins[username].channels;
 
+/* get the admin level of a user in a given channel */
 const getAdminLevel = (state, username, channel) => state.admins[username].channels[channel].admin_level;
 
+/* check if a user is an admin in a given channel */
 const isAdmin = (state, username, channel) => Object.keys(state.admins[username].channels).includes(channel);
 
 export {
--- src/state/ducks/polls/selectors.js
+++ src/state/ducks/polls/selectors.js
@@ -1,7 +1,10 @@
+/* get a poll object from a given poll_id */
 const getPollById = (state, poll_id) => state.polls[poll_id];
 
+/* get the name of a given poll */
 const getPollName = (state, poll_id) => state.polls[poll_id].poll_name;
 
+/* get the highest voted option for a given poll */
 const getTopOption = (state, poll_id) => {
   try {
     const result = getPollOptions(state, poll_id)
@@ -19,25 +22,48 @@ const getTopOption = (state, poll_id) => {
   }
 };
 
+/* get all option objects for a given poll_id */
 const getPollOptions = (state, poll_id) => {
   return state.polls[poll_id].poll_options;
 }
+
+/* get a list of all option names for a given poll_id */
 const getOptionNames = (state, poll_id) => state.polls[poll_id].poll_options.map(option => option.option_name).join(', ');
 
+/* get the number of options for a given poll_id */
 const getNumOptions = (state, poll_id) => state.polls[poll_id].poll_options.length;
 
+/* check if a poll is open */
 const pollIsOpen = (state, poll_id) => state.polls[poll_id].poll_is_open;
 
+/* get an option from its id number */
 const getOptionById = (state, poll_id, option_id) => state.polls[poll_id].poll_options[option_id];
 
+/* get a list of polls available */
 const listPolls = (state) => state.polls.map((poll) => ({ poll_id: state.polls.indexOf(poll), poll_name: poll.poll_name }) );
 
+/* get the ID associated with an option from its name */
 const getOptionIdByName = (state, poll_id, option_name) => {
   return getPollOptions(state, poll_id)
     .map(option => {
       if(option.option_name.toLowerCase() === option_name.toLowerCase())
         return option.option_id;
     })[0];
+};
+
+/* check if a given poll_id is valid */
+const pollExists = (state, poll_id) => {
+  return !(typeof state.polls[poll_id] === 'undefined');
+}
+
+/* get the option_id of a users vote */
+const getUserVoteID = (state, poll_id, username) => {
+  state.polls[poll_id].poll_options
+    .forEach(option => {
+      if(option.option_voters.includes(username))
+        return option.option_id;
+    });
+  return -1;
 };
 
 export {
@@ -51,4 +77,5 @@ export {
   getTopOption,
   listPolls,
   getOptionIdByName,
+  pollExists,
 };
