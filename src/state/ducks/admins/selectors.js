35f84643e1d157089da32ead479b760e027f767b
Durendal <sir.durendal@gmail.com>
2020-01-06 13:59:10 -0600

    Shorter lines

--- src/state/ducks/admins/selectors.js
+++ src/state/ducks/admins/selectors.js
@@ -2,10 +2,13 @@
 const getAdminChannels = (state, username) => state.admins[username].channels;
 
 /* get the admin level of a user in a given channel */
-const getAdminLevel = (state, username, channel) => state.admins[username].channels[channel].admin_level;
-
+const getAdminLevel = (state, username, channel) => (
+  state.admins[username].channels[channel].admin_level
+)
 /* check if a user is an admin in a given channel */
-const isAdmin = (state, username, channel) => Object.keys(state.admins[username].channels).includes(channel);
+const isAdmin = (state, username, channel) => (
+  return Object.keys(state.admins[username].channels).includes(channel)
+)
 
 export {
   getAdminChannels,
