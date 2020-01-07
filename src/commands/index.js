1d36bc8adde5238cf5a28648db080590a3878e4c
Durendal <sir.durendal@gmail.com>
2020-01-06 23:44:59 -0600

    Added dynamic module loading and moved commands into DUCKS modules

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,12 +1,40 @@
-import * as admins from 'src/commands/admins';
-import * as polls from 'src/commands/polls';
+const { readdirSync, statSync } = require('fs');
+const { join } = require('path');
+//import * as admins from 'src/commands/admins';
+//import * as polls from 'src/commands/polls';
 import { client } from 'src/utils/client';
+import "regenerator-runtime/runtime";
 
 /**
   Object containing all commands for the bot.
  */
 const commands = {};
 
+const loadCommands = async() => {
+  if(!Object.keys(commands).length) {
+    try {
+      console.log(__dirname);
+      const base_dir = __dirname.split('/').slice(0, -1).join('/');
+      const dirs = readdirSync(`${base_dir}/state/ducks/`).filter(f => statSync(join(`${base_dir}/state/ducks/`, f)).isDirectory());
+      console.log(dirs);
+      dirs.forEach(async dir => {
+        commands[dir] = await import(`${base_dir}/state/ducks/${dir}/commands`);
+        Object.keys(commands[dir])
+          .forEach(command => add(command, commands[dir][command]));
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return commands;
+}
+
+try {
+  console.log("Pre-load:", JSON.stringify(commands));
+  loadCommands().then(res => console.log("Post-load:", JSON.stringify(commands)));
+} catch (error) {
+  console.log(error);
+}
 /**
   Add a command to the commands list.
   @param {String} name - The name of the command
@@ -30,7 +58,7 @@ const listCommands = (target) => {
 
 // Add commands to the commands object
 add('list', listCommands);
-
+/*
 Object.keys(polls)
   .forEach(poll => {
     add(poll, polls[poll]);
@@ -40,7 +68,7 @@ Object.keys(admins)
   .forEach(admin => {
     add(admin, admins[admin]);
   });
-
+*/
 /**
   Execute an issued command
   @param {String} name - The command to execute
@@ -57,9 +85,10 @@ const commandSwitch = (name, msg, context, target) => {
 };
 
 export {
-  admins,
-  polls,
+  //admins,
+  //polls,
   commands,
   commandSwitch,
   add,
+  loadCommands,
 };
