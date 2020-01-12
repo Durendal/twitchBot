e5c9aa17e96038f67ed90dd9db3da5296b21388b
Durendal <sir.durendal@gmail.com>
2020-01-12 17:17:54 -0600

    Updates for commands module

--- src/state/ducks/index.js
+++ src/state/ducks/index.js
@@ -3,4 +3,6 @@ export { default as polls } from './polls';
 export { default as smedia } from './smedia';
 export { default as trivia } from './trivia';
 export { default as bot } from './bot';
+export { default as mgames } from './mgames';
+export { default as commands } from './commands';
 export { default as scraper } from './scraper';
